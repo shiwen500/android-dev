1046da277ef796d1fe6b47c0b2378ede3bf8c89a
seven <471599051@qq.com>
2015-12-25 11:18:26 +0800

    publish the library to jcenter, and using it in the the sample application.

--- app/src/main/java/com/github/shiwen500/android_dev/MainActivity.java
+++ app/src/main/java/com/github/shiwen500/android_dev/MainActivity.java
@@ -1,5 +1,6 @@
 package com.github.shiwen500.android_dev;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.github.shiwen500.library.data.UriHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,6 +50,10 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+        UriHelper.getPath(this, Uri.parse(""));
+
         return super.onOptionsItemSelected(item);
     }
+
+
 }
